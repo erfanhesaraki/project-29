72236425c431c093e0bc862ab373c9c21212d919
Erfan hesaraki <120978791+erfanhesaraki@users.noreply.github.com>
2023-02-21 23:00:31 +0330

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,5 @@
+function _toggle() {
+  document.querySelector(".night>span").classList.toggle("nightmood");
+  document.querySelector(".night>span").classList.toggle("_spanNight");
+  document.getElementsByTagName("body")[0].classList.toggle("mainToggle");
+}
